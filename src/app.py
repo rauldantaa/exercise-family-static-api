986b478ecc85db4ff8b244209b8048cb82c80f76
Raul Danta <rdanta09@gmail.com>
2025-07-09 17:12:48 +0000

    api-estatica

--- src/app.py
+++ src/app.py
@@ -6,40 +6,130 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-# from models import Person
+
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# Create the jackson family object
-jackson_family = FamilyStructure("Jackson")
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String(80), unique=True, nullable=False)
+    email = Column(String(120), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    firstname = Column(String(100), nullable=True)
+    lastname = Column(String(100), nullable=True)
+    fecha_subscripcion = Column(DateTime, default=datetime.utcnow)
+    is_active = Column(Boolean, default=True)
+    
+   
+    favoritos_personajes = relationship("FavoritoPersonaje", back_populates="usuario")
+    favoritos_planetas = relationship("FavoritoPlaneta", back_populates="usuario")
+    posts = relationship("Post", back_populates="autor")
+    comentarios = relationship("Comentario", back_populates="autor")
+
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(100), nullable=False)
+    altura = Column(String(20), nullable=True)
+    peso = Column(String(20), nullable=True)
+    color_cabello = Column(String(50), nullable=True)
+    color_piel = Column(String(50), nullable=True)
+    color_ojos = Column(String(50), nullable=True)
+    ano_nacimiento = Column(String(20), nullable=True)
+    genero = Column(String(20), nullable=True)
+    planeta_origen_id = Column(Integer, ForeignKey('planeta.id'), nullable=True)
+    descripcion = Column(Text, nullable=True)
+    
+    
+    planeta_origen = relationship("Planeta", back_populates="personajes_nativos")
+    favoritos = relationship("FavoritoPersonaje", back_populates="personaje")
+
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(100), nullable=False)
+    periodo_rotacion = Column(String(20), nullable=True)
+    periodo_orbital = Column(String(20), nullable=True)
+    diametro = Column(String(20), nullable=True)
+    clima = Column(String(100), nullable=True)
+    gravedad = Column(String(20), nullable=True)
+    terreno = Column(String(100), nullable=True)
+    agua_superficial = Column(String(20), nullable=True)
+    poblacion = Column(String(50), nullable=True)
+    descripcion = Column(Text, nullable=True)
+    
+   
+    personajes_nativos = relationship("Personaje", back_populates="planeta_origen")
+    favoritos = relationship("FavoritoPlaneta", back_populates="planeta")
+
+class FavoritoPersonaje(Base):
+    __tablename__ = 'favorito_personaje'
+    
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'), nullable=False)
+    personaje_id = Column(Integer, ForeignKey('personaje.id'), nullable=False)
+    fecha_agregado = Column(DateTime, default=datetime.utcnow)
+    
+   
+    usuario = relationship("Usuario", back_populates="favoritos_personajes")
+    personaje = relationship("Personaje", back_populates="favoritos")
+
+class FavoritoPlaneta(Base):
+    __tablename__ = 'favorito_planeta'
+    
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'), nullable=False)
+    planeta_id = Column(Integer, ForeignKey('planeta.id'), nullable=False)
+    fecha_agregado = Column(DateTime, default=datetime.utcnow)
+    
+   
+    usuario = relationship("Usuario", back_populates="favoritos_planetas")
+    planeta = relationship("Planeta", back_populates="favoritos")
+
+class Post(Base):
+    __tablename__ = 'post'
+    
+    id = Column(Integer, primary_key=True)
+    titulo = Column(String(200), nullable=False)
+    contenido = Column(Text, nullable=False)
+    fecha_creacion = Column(DateTime, default=datetime.utcnow)
+    fecha_actualizacion = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    autor_id = Column(Integer, ForeignKey('usuario.id'), nullable=False)
+    is_published = Column(Boolean, default=True)
+    
+   
+    autor = relationship("Usuario", back_populates="posts")
+    comentarios = relationship("Comentario", back_populates="post")
+
+class Comentario(Base):
+    __tablename__ = 'comentario'
+    
+    id = Column(Integer, primary_key=True)
+    contenido = Column(Text, nullable=False)
+    fecha_creacion = Column(DateTime, default=datetime.utcnow)
+    autor_id = Column(Integer, ForeignKey('usuario.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    
+ 
+    autor = relationship("Usuario", back_populates="comentarios")
+    post = relationship("Post", back_populates="comentarios")
 
 
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
+def to_dict(self):
+    return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
-# Generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
-
-
-@app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
-
-
-
-# This only runs if `$ python src/app.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+Usuario.to_dict = to_dict
+Personaje.to_dict = to_dict
+Planeta.to_dict = to_dict
+FavoritoPersonaje.to_dict = to_dict
+FavoritoPlaneta.to_dict = to_dict
+Post.to_dict = to_dict
+Comentario.to_dict = to_dict
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,45 +1,81 @@
-"""
-Update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
-
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
         self._next_id = 1
-        self._members = [
+        self._members = []
+        
+        
+        self._initialize_family()
+
+    def _initialize_family(self):
+        
+        initial_members = [
             {
-                "id": self._generate_id(),
                 "first_name": "John",
-                "last_name": last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "first_name": "Jane",
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "first_name": "Jimmy",
+                "age": 5,
+                "lucky_numbers": [1]
             }
         ]
+        
+        for member in initial_members:
+            self.add_member(member)
 
-    # This method generates a unique incremental ID
+    
     def _generate_id(self):
         generated_id = self._next_id
         self._next_id += 1
         return generated_id
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
-        pass
+        """
+        
+        """
+        
+        if 'id' not in member or member['id'] is None:
+            member['id'] = self._generate_id()
+        else:
+            
+            if member['id'] >= self._next_id:
+                self._next_id = member['id'] + 1
+        
+        
+        member['last_name'] = self.last_name
+        
+        
+        self._members.append(member)
+        
+        return member
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        """
+       
+        """
+        for i, member in enumerate(self._members):
+            if member['id'] == id:
+                return self._members.pop(i)
+        return None
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+        """
+       
+        """
+        for member in self._members:
+            if member['id'] == id:
+                return member
+        return None
 
-    # This method is done, it returns a list with all the family members
     def get_all_members(self):
+        """
+       
+        """
         return self._members
